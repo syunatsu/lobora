751a0cda2c8082e9a98b3ca3b516bfdc9ef4f47e
syunatsu <myf.tyys@gmail.com>
2022-05-18 16:34:52 +0900

    ActiveHashの導入

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -4,7 +4,10 @@ class Menu < ApplicationRecord
   validates :recipe, presence: true, length: { maximum: 400 }
   validates :how_to, presence: true, length: { maximum: 1000 }
   validates :price, presence: true, format: {with: /\A[0-9]+\z/}
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id, numericality: { other_than: 0, message: "can't be blank" }
 
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
 end
