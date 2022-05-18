f4206fec781e441ed2aa1878dba154f4f0de49dd
syunatsu <myf.tyys@gmail.com>
2022-05-18 15:25:40 +0900

    誤記修正

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,7 +3,7 @@ class Menu < ApplicationRecord
   validates :explain, presence: true, length: { maximum: 250 }
   validates :recipe, presence: true, length: { maximum: 400 }
   validates :how_to, presence: true, length: { maximum: 1000 }
-  validates :price, presence: true, format {with: /\A[0-9]+\z/}
+  validates :price, presence: true, format: {with: /\A[0-9]+\z/}
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
 
   belongs_to :user
