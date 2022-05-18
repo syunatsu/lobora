e14b8de7d545fbb98144f5ef3d0cbfb798a6ab1b
syunatsu <myf.tyys@gmail.com>
2022-05-18 16:10:32 +0900

    new、createアクションの実装

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,4 +1,22 @@
 class MenusController < ApplicationController
   def index
   end
+
+  def new
+    @menu = Menu.new
+  end
+
+  def create
+    @menu = Menu.new(menu_params)
+    if @menu.save
+      redirect to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def menu_params
+    require.(:menu).permit(:name, :explain, :recipe, :how_to, :price, :cotegory_id, :image).merge(user_id: current_user.id)
+  end
 end
