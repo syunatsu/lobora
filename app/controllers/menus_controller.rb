93102a0295eeb70eaf7f48c4d0a1dde65c476a3c
syunatsu <myf.tyys@gmail.com>
2022-05-19 15:15:00 +0900

    パスの変更

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,5 +1,14 @@
 class MenusController < ApplicationController
+
+  def top
+  end
+
   def index
+    @foods = Menu.find_by(category_id: 1)
+    @dolces = Menu.find_by(category_id: 2)
+    @drinks = Menu.find_by(category_id: 3)
+
+    render "menus/_#{params[:name]}", locals: {foods: @foods, dolces: @dolces, drinks: @drinks}
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "menus#index"
-  resources :menus, only: [:index, :new, :create]
+  root to: "menus#top"
+  resources :menus, only: [:new, :create]
+  get 'menus/:name', controller: 'menus', action: 'index'
 end
