ba3c478296c7ab37fd75a20e00c7dbc8d7e524ce
syunatsu <myf.tyys@gmail.com>
2022-05-19 11:10:00 +0900

    ActiveStrageの導入

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 4.0.0'
   gem 'factory_bot_rails'
+  gem 'faker'
 
 end
 
@@ -59,3 +60,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'pry-rails'
 gem 'active_hash'
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -5,9 +5,15 @@ class Menu < ApplicationRecord
   validates :how_to, presence: true, length: { maximum: 1000 }
   validates :price, presence: true, format: {with: /\A[0-9]+\z/}
   validates :category_id, numericality: { other_than: 0, message: "can't be blank" }
+  validate :validate_image
 
   belongs_to :user
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+
+  def validate_image
+    errors.add(:image, 'を1枚添付してください') unless image.attached?
+  end
 end
