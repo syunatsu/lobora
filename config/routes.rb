ce3fa272cf16dcd43f24cd89d555a824e426eee8
syunatsu <myf.tyys@gmail.com>
2022-05-20 11:02:19 +0900

    ルーティング実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "menus#top"
-  resources :menus, only: [:new, :create]
+  resources :menus, only: [:new, :create, :show]
   get 'menus/:name', controller: 'menus', action: 'index'
 end
