945d0bbb337661e87d92dc6a0c591e41b20cc05f
syunatsu <myf.tyys@gmail.com>
2022-05-27 10:36:54 +0900

    showページのedit,deleteボタン修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+[
+  ['natsumi@mail', 'natsumi', '111111'],
+  ['syota@mail', 'syota', '111111']
+].each do |mail, name, password|
+  User.create!(
+    { email: mail, nickname: name, password: password }
+  )
+end
