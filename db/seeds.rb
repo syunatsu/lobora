59dadd5d58a041d48b0303d62654920e181542f2
syunatsu <myf.tyys@gmail.com>
2022-05-27 12:01:05 +0900

    画像追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
-[
-  ['natsumi@mail', 'natsumi', '111111'],
-  ['syota@mail', 'syota', '111111']
-].each do |mail, name, password|
-  User.create!(
-    { email: mail, nickname: name, password: password }
-  )
-end
+# ユーザーの作成
+# [
+#   ['natsumi@mail', 'natsumi', '111111'],
+#   ['syota@mail', 'syota', '111111']
+# ].each do |mail, name, password|
+#   User.create!(
+#     { email: mail, nickname: name, password: password }
+#   )
+# end
